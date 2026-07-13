1334461763b78b273468c3bcc88bb8f3234dfabc
MehulParmar11 <mehulparmar8128113027@gmail.com>
2026-07-13 17:00:23 +0530

    Delete backend directory

--- backend/src/routes/bmiRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { saveBMI, getBMIHistory } = require("../controllers/bmiController");
-const auth = require("../middleware/auth"); // your JWT middleware
-
-router.post("/bmi", auth, saveBMI);
-router.get("/bmi", auth, getBMIHistory);
-
-module.exports = router;
--- backend/src/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
-const chatController = require("../controllers/chatController");
-
-router.post("/send", authMiddleware, chatController.sendMessage);
-router.get("/:receiverId", authMiddleware, chatController.getMessages);
-
-module.exports = router;
